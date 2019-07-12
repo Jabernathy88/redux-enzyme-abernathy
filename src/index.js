ce8f2bc326b644f23b099481c97a4ad809b86483
Jeremy Abernathy <j.e.abernathy@gmail.com>
2019-07-12 06:27:34 -0400

    Scaling down folder structure, temporarily.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import { Provider } from 'react-redux';
-import store from './store';
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import rootReducer from "./rootReducer";
+
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello from App.js</h2>
+    </div>
+  );
+}
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk)
+)
+const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-  (
-    <Provider store={store}>
-      <App />
-    </Provider>
-  ), document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
 );
